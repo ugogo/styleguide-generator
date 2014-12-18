9cf726ad1f552517354118e947319e42ecd1fe0b
onaliugo <hello@onali-ugo.fr>
2014-12-18 16:56:23 +0100

    First draft v0.1.0

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-module.exports = function(){
+var fs = require('fs');
+var del = require('del');
+var glob = require('glob');
+var mkdirp = require('mkdirp');
+var mdConvert = require('namp');
+
+module.exports = {
+	generate: function(opts){
+
+		/*
+		 * Clear baseFolder
+		 */
+		del(opts.distFolder, function(){
+
+			/*
+			 * For each MD files into baseFolder
+			 */
+			glob(opts.baseFolder + '**/*.md', function(err, files){
+				files.forEach(function(path, i){
+					fs.readFile(path, 'utf8', function(err, fileString) {
+
+						/*
+						 * Convert it to .html
+						 */
+						var html = mdConvert(fileString).html;
+						var folderPath = path.replace(opts.baseFolder, '').split('/')[0];
+						var fileName = path.replace(opts.baseFolder, '').split('/')[1].replace('.md', '.html');
+						var filePath = opts.distComponentFolder + folderPath + '/' + fileName;
+
+						/*
+						 * Create a folder
+						 * append it distFolder
+						 * append currentFile into the currentFolder
+						 */
+						mkdirp(opts.distComponentFolder + folderPath, function(){
+							fs.writeFile(filePath, html);
+						});
+					});
+				});
+			});
+		});
+	}
 };
